5e786de9287e95598fcc460176c61361530dfb73
rishabh08 <rishabh.parashar8@gmail.com>
2022-07-01 12:09:20 +0530

    Project Intialize

---
+++ src/test/java/com/retail/bank/retailbank/RetailBankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.retail.bank.retailbank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RetailBankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
